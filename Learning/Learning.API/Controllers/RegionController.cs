aa26ccb4b326a86529455332c885778a931d57fd
rp53218528 <rp53218528@gmail.com>
2023-10-27 18:17:50 +0530

    CRUD operation

--- Learning/Learning.API/Controllers/RegionController.cs
+++ Learning/Learning.API/Controllers/RegionController.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
+using Learning.API.Data;
 using Learning.API.Models.Domain;
+using Learning.API.Models.DTO;
 using Learning.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Learning.API.Controllers
 {
@@ -19,7 +22,7 @@ namespace Learning.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllRegions()
+        public async Task<IActionResult> GetAllRegionsAsync()
         {
             var regions = await regionRepository.GetAllAsync();
 
@@ -44,6 +47,117 @@ namespace Learning.API.Controllers
             //Automapper
             var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
             return Ok(regionsDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetRegionsAsync")]
+        public async Task<IActionResult> GetRegionsAsync(Guid id)
+        {
+            var regions = await regionRepository.GetAsync(id);
+            var regionDTO = mapper.Map<Models.DTO.Region>(regions);
+
+            if (regions == null)
+            {
+                return NotFound();
+            }
+            return Ok(regionDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRegionAsync(Models.DTO.AddRegionRequest addRegionRequest)
+        {
+            //Request(DTO) to Domain model 
+            var region = new Models.Domain.Region()
+            {
+                Code = addRegionRequest.Code,
+                Area = addRegionRequest.Area,
+                Lat = addRegionRequest.Lat,
+                Long = addRegionRequest.Long,
+                Name = addRegionRequest.Name,
+                Population = addRegionRequest.Population
+            };
+            //Pass details to repository
+            region = await regionRepository.AddAsync(region);
+
+            //convert back to DTO
+            var regionDto = new Models.DTO.Region
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Name = region.Name,
+                Population = region.Population
+            };
+            return CreatedAtAction(nameof(GetRegionsAsync), new { id = regionDto.Id }, regionDto);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteRegionAsync(Guid id)
+        {
+            //Get region from database
+            var region = await regionRepository.DeleteAsync(id);
+
+            //If Null Notfound
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            //Convert response back to DTO
+            var regionDTO = new Models.DTO.Region
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Name = region.Name,
+                Population = region.Population
+            };
+
+            //Return Ok response
+            return Ok(regionDTO);
+        }
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateRegionAsync([FromBody] Models.DTO.UpdateRegionRequest updateRegionRequest, [FromRoute] Guid id)
+        {
+            //convert DTo to domain model
+            var region = new Models.Domain.Region()
+            {
+                Code = updateRegionRequest.Code,
+                Area = updateRegionRequest.Area,
+                Lat = updateRegionRequest.Lat,
+                Long = updateRegionRequest.Long,
+                Name = updateRegionRequest.Name,
+                Population = updateRegionRequest.Population
+            };
+
+            //update Region using repository
+            region = await regionRepository.UpdateAsync(id, region);
+
+            //If null then NotFound
+            if (region == null)
+            {
+                return NotFound();
+            }
+            //Convert Domain back to DTo
+            var regionDto = new Models.DTO.Region
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Name = region.Name,
+                Population = region.Population
+            };
+            //Return Ok response
+            return Ok(regionDto);
         }
     }
 }
--- Learning/Learning.API/Repositories/RegionRepository.cs
+++ Learning/Learning.API/Repositories/RegionRepository.cs
@@ -9,11 +9,57 @@ namespace Learning.API.Repositories
         private readonly LearningDbContext learningDbContext;
         public RegionRepository(LearningDbContext learningDbContext)
         {
-                this.learningDbContext = learningDbContext;
+            this.learningDbContext = learningDbContext;
         }
+
+        public async Task<Region> AddAsync(Region region)
+        {
+            region.Id = Guid.NewGuid();
+            await learningDbContext.AddAsync(region);
+            await learningDbContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> DeleteAsync(Guid id)
+        {
+            var region = await learningDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if (region == null)
+            {
+                return null;
+            }
+            //Delete the region 
+            learningDbContext.Regions.Remove(region);
+            await learningDbContext.SaveChangesAsync();
+            return region;
+        }
+
         public async Task<IEnumerable<Region>> GetAllAsync()
         {
             return await learningDbContext.Regions.ToListAsync();
+        }
+
+        public async Task<Region> GetAsync(Guid id)
+        {
+            var region = await learningDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            return region;
+        }
+
+        public async Task<Region> UpdateAsync(Guid id, Region region)
+        {
+            var existingRegion = await learningDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingRegion == null)
+            {
+                return null;
+            }
+            existingRegion.Code = region.Code;
+            existingRegion.Name = region.Name;
+            existingRegion.Area = region.Area;
+            existingRegion.Lat = region.Lat;
+            existingRegion.Long = region.Long;
+            existingRegion.Population = region.Population;
+
+            await learningDbContext.SaveChangesAsync();
+            return existingRegion;
         }
     }
 }
